51ad8d742247f06ce483dcb32760f3d26ba02744
Kiran Tirunagiri <me190003064@iiti.ac.in>
2021-04-25 18:00:37 +0530

    Some recent changes while starting to learn react again.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import React from "react";
-// import { component } from "react-dom";
+import "./App.css";
 
 function App() {
-	return <h1>this is nice</h1>;
+  return (
+    <div className="App">
+      <div className="content">
+        <h1>App component</h1>
+      </div>
+      <div className="picture" id="picture-1">
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo
+          temporibus corporis sequi eligendi culpa nisi dicta recusandae dolor
+          qui! Explicabo nihil consectetur dignissimos recusandae magnam unde
+          eligendi saepe ullam soluta?
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default App;
