47f5a8c5d561b63f9515bdea0e90aaf8ebfe9310
Kiran Tirunagiri <me190003064@iiti.ac.in>
2021-05-18 01:52:14 +0530

    Added in firebase credentials

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDMeUokOAA94Eh1ll7C_KmicP6yuwO4Z8Q",
+    authDomain: "whatsapp-codfish.firebaseapp.com",
+    projectId: "whatsapp-codfish",
+    storageBucket: "whatsapp-codfish.appspot.com",
+    messagingSenderId: "416030160497",
+    appId: "1:416030160497:web:d6fa8fbd4c888b2b6b8dd0",
+    measurementId: "G-VX3XZB4Y0J",
+};
