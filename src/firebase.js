e9e1ad4cdfc45d4e10541176bb6560cafd5db4e1
Kiran Tirunagiri <me190003064@iiti.ac.in>
2021-05-18 21:21:05 +0530

    Added firebase locally

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,5 @@
+import firebase from "firebase";
+
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
     apiKey: "AIzaSyDMeUokOAA94Eh1ll7C_KmicP6yuwO4Z8Q",
@@ -8,3 +10,11 @@ const firebaseConfig = {
     appId: "1:416030160497:web:d6fa8fbd4c888b2b6b8dd0",
     measurementId: "G-VX3XZB4Y0J",
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth()l
+const provider = new firebase.auth.GoogleAuthProvider();
+
+export {auth , provider};
+export default db;
