1c31b93b78bff8da045a3e8bc92b1fe417a3f668
Kiran Tirunagiri <me190003064@iiti.ac.in>
2021-05-19 01:40:54 +0530

    The final firebase config

--- src/firebase.js
+++ src/firebase.js
@@ -13,8 +13,8 @@ const firebaseConfig = {
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 const db = firebaseApp.firestore();
-const auth = firebase.auth()l
+const auth = firebase.auth();
 const provider = new firebase.auth.GoogleAuthProvider();
 
-export {auth , provider};
+export { auth, provider };
 export default db;
